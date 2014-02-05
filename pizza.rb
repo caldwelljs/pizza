63a3821487198414585782d793d7be0b0226e5ee
John Caldwell <caldwelljs@gmail.com>
2014-02-04 22:41:51 -0600

    delete code for clean run at redoing lesson

--- pizza.rb
+++ pizza.rb
@@ -1,19 +1,10 @@
 module Pizza
   
   class Pie
-    attr_accessor :toppings
-
-    def  initialize(toppings = [Topping.new("cheese", vegetarian: true)])
-      @toppings = toppings
-    end
+    
   end
 
   class Topping
-  	attr_accessor :name, :vegetarian
-
-  	def initialize(name, vegetarian: false)
-  		@name = name
-      @vegetarian = vegetarian
-  	end
+  	
   end
 end
